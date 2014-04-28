6dfbed760e8f3e080627873a2f13083eed26961b
jhonny <steven.alvarez.v@gmail.com>
2014-04-28 03:19:50 -0400

    cambios

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 /************************************ VARIABLES DE CONFIGURACION *******************************************************/
 
 /************************************ server *******************************************************/
-//var BASE_URL_APP = "http://www.queplanmadrid.es/";
+var BASE_URL_APP = "http://terrazas.arrobacreativa.com/";
 
 /************************************ localhost *******************************************************/
-var BASE_URL_APP = "http://localhost/lineup/web/";
+//var BASE_URL_APP = "http://localhost/lineup/web/";
