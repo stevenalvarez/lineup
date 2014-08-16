1d730e5fb782e641a29f496662ceb7542ce2812f
jhonny <steven.alvarez.v@gmail.com>
2014-08-16 03:24:35 -0400

    fix

--- www/js/app.functions.js
+++ www/js/app.functions.js
@@ -466,7 +466,7 @@ function getMenuFooter(parent_id,filtro_id){
                                         }else if(activado == "off"){
                                             off = 'selected="selected"';
                                         }
-                                        var cls = "al"+index+i;
+                                        var cls = "aljhonny"+index+i;
                                         if(clase == "pubs" || clase == "promos"){
                                             title = IDIOMA == "castellano" ? "Recibir alertas" : "Receive alerts";
                                         }
@@ -489,7 +489,7 @@ function getMenuFooter(parent_id,filtro_id){
                                         if(index == "sesiones") index = "sesions";
                                         if(index == "festivales") index = "festivals";
                                         $.each(item, function(i, t) {
-                                            var cls = "al"+index+i;
+                                            var cls = "aljhonny"+index+i;
                                             var imagen_redonda = t.imagen_redonda!=""?t.imagen_redonda:"default.png";
                                             var css = "";
                                             if(index == "pubs" || index == "promos"){
@@ -611,7 +611,7 @@ function loadMoreItems(container, usuario_id){
                                 }else if(activado == "off"){
                                     off = 'selected="selected"';
                                 }
-                                var cls = "al"+index+i;
+                                var cls = "alpaola"+index+i;
                                 if(clase == "pubs" || clase == "promos"){
                                     title = IDIOMA == "castellano" ? "Recibir alertas" : "Receive alerts";
                                 }
@@ -633,7 +633,7 @@ function loadMoreItems(container, usuario_id){
                                 if(index == "sesiones") index = "sesions";
                                 if(index == "festivales") index = "festivals";
                                 $.each(item, function(i, t) {
-                                    var cls = "al"+index+i;
+                                    var cls = "alpaola"+index+i;
                                     var imagen_redonda = t.imagen_redonda!=""?t.imagen_redonda:"default.png";
                                     var css = "";
                                     if(index == "pubs" || index == "promos"){
